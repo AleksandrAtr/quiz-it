be4097bfa856514dd7d8b8b84f89da9f660de19d
Aleks Atr <atraleks@gmail.com>
2023-10-17 19:57:32 +0100

    feat: main.js updated quiz logic

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,66 +1,109 @@
 //import quiz questions
-import {questions} from './questions.js';
+import { questions } from './questions.js';
 
-// start quize f
-    // to statrt the quiz we need
-        //show question
-        //show answers
-        //show question number
-        //show total questions
-
-// get question f
-// swow answers f
-// reset answers f
-// get global variable questions index
-// get global variable questions index
-
-const startQuizBtn = document.getElementById('menu-btn');
+const start = 'Start';
+const next = 'Next';
+const questionsLength = questions.length;
+let menuButton = document.getElementById('menu-btn');
 let quizQuestion = document.getElementById('cdm-question');
 let quizAnswers = document.getElementById('cdm-answers');
-
+const answerButtons = document.getElementsByClassName('btn');
 
 let questionIndex = 0;
 let score = 0;
 
+menuButton.addEventListener('click', startQuiz);
+
 function resetQuestionContainer() {
+    menuButton.style.display = 'none';
     quizQuestion.innerHTML = "";
     quizAnswers.innerHTML = "";
 }
 
-
-startQuizBtn.addEventListener('click', startQuiz);
-
-function startQuiz() {
-    document.getElementById('cdm-quiz-container').style.display = "block";
-    startQuizBtn.innerHTML = 'Next';
-    resetQuestionContainer();
-    showQuestion();
-    // console.log(quizQuestion);
-    // console.log(quizAnswers);
-
-}
-
-
 function showQuestion() {
+    resetQuestionContainer();
     // declare a constant with assigned value from the questions array (imported from module)
-    const currentQuestion = questions[questionIndex]['question']; 
+    const currentQuestion = questions[questionIndex]['question'];
     // create a template for the html question element
     const questionTemplate = `<h2>${currentQuestion}</h2>`;
     // update html question element with current question
     quizQuestion.innerHTML = questionTemplate;
 
 
-    // declare a constant with assigned value from the questions array (imported from module)
+    // declare a constant with assigned value from the questions/answers arrays (imported from module)
     const currentAnswer = questions[questionIndex]['answers'];
-    for (const value in currentAnswer){
+    // create possible answers for the current question
+    for (const value in currentAnswer) {
         let answer = currentAnswer[value]['answer'];
-        const answerTemplate = `<button class='btn'>${answer}</button>`;
+        let isCorrect = currentAnswer[value]['correct'];
+        //console.log("I am 'correct' in the currentAnswer - ", correct)
+        const answerTemplate = `
+            <button correct="${isCorrect}" class='btn'>${answer}</button>
+        `;
         quizAnswers.innerHTML += answerTemplate;
-    }   
+    }
+    applyEventListener();
+}
+
+function applyEventListener() {
+    const answerButtons = document.getElementsByClassName('btn');
+    for (let i = 0; i < answerButtons.length; i++) {
+        answerButtons[i].addEventListener('click', checkAnswer);
+    }
+}
+
+function checkAnswer(event) {
+    // declare selected button with event taget value
+    const selectedBtn = event.target;
+    const correctAnswer = selectedBtn.getAttribute('correct') === 'true';
+    if (correctAnswer) {
+        // selectedBtn.classList.add('correct');
+        score += 1;
+    } else {
+        selectedBtn.classList.add('incorrect');
+    }
+
+    const array = Array.prototype.slice.call(answerButtons);
+    array.forEach(button => {
+        if (button.getAttribute('correct') === 'true') {
+            button.classList.add('correct');
+        }
+        button.disabled = true;
+    })
+    questionIndex += 1;
+    // console.log(questionIndex);
+    menuButton.style.display = 'block';
+    // debugger;
+    menuButton.removeEventListener('click', startQuiz);
+    menuButton.addEventListener('click', () => {handleMenuButton(next)});
+}
+
+function showResult() {
+    resetQuestionContainer();
+    
+    quizAnswers.innerHTML = "Quiz completed";
+}
+
+function handleMenuButton(type) {
+    // you can use ternarry here
+    if (type == start) {
+        // resetQuestionContainer();
+        showQuestion();
+        document.getElementById('cdm-quiz-container').style.display = "block";
+        menuButton.innerHTML = 'Next';
+    } else {
+        if (questionIndex < (questionsLength)) {
+            showQuestion();
+        } else { showResult()}
+        // (questionIndex < questionsLength) ? showQuestion : showResult;
+    }
 }
 
 
 
+function startQuiz() {
+    handleMenuButton(start);
+}
 
 
 
