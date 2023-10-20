2e7653077e4611261fa495c5e61ce6949be43f30
Aleks Atr <atraleks@gmail.com>
2023-10-20 12:47:51 +0100

    chore: questions.js update questions

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,45 +1,215 @@
 export const questions = [
     {
-        question : "Under CDM 2015, who is primarily responsible for coordinating health and safety during the construction phase of a project?",
-        answers : [
+        question: "In CDM 2015, who is responsible for compiling the health and safety file?",
+        answers: [
             {
-                answer : "Client",
-                correct : false
+                answer: "Principal Contractor",
+                correct: false
             },
             {
-                answer : "Principal Designer",
-                correct : false
+                answer: "HSE inspector",
+                correct: true
             },
             {
-                answer : "Designer",
-                correct : false
+                answer: "Client",
+                correct: false
             },
             {
-                answer : "Principal Contractor",
-                correct : true
+                answer: "Principal Designer",
+                correct: false
             }
         ]
     },
     {
-        question : "According to CDM 2015, who is responsible for compiling the health and safety file?",
-        answers : [
+        question: "What does CDM stand for in CDM 2015?",
+        answers: [
             {
-                answer : "Principal Contractor",
-                correct : false
-            }, {
-                answer : "Project Manager",
-                correct : false
+                answer: "Client Design Management",
+                correct: false
             },
             {
-                answer : "HSE inspector",
-                correct : false
+                answer: "Construction Design Management",
+                correct: true
             },
             {
-                answer : "Principal Designer",
-                correct : true
+                answer: "Critical Design Management",
+                correct: false
+            },
+            {
+                answer: "Construction Development Management",
+                correct: false
+            }
+        ]
+    },
+    {
+        question: "Which document provides information about health and safety hazards before the construction phase in CDM 2015?",
+        answers: [
+            {
+                answer: "Construction Phase Plan (CPP)",
+                correct: false
+            },
+            {
+                answer: "Health and Safety File",
+                correct: false
+            },
+            {
+                answer: "Pre-Construction Information (PCI)",
+                correct: true
+            },
+            {
+                answer: "Risk Assessment Report (RAR)",
+                correct: false
+            }
+        ]
+    },
+    {
+        question: "Who is responsible for coordinating health and safety during the design phase of a project according to CDM 2015?",
+        answers: [
+            {
+                answer: "Client",
+                correct: false
+            },
+            {
+                answer: "Principal Contractor",
+                correct: false
+            },
+            {
+                answer: "Designer",
+                correct: false
+            },
+            {
+                answer: "Principal Designer",
+                correct: true
+            }
+        ]
+    },
+    {
+        question: "What does the 'HSE' stand for in CDM 2015?",
+        answers: [
+            {
+                answer: "Health and Safety Executive",
+                correct: true
+            },
+            {
+                answer: "Health and Security Environment",
+                correct: false
+            },
+            {
+                answer: "High Safety Efficiency",
+                correct: false
+            },
+            {
+                answer: "Hazardous Site Evaluation",
+                correct: false
+            }
+        ]
+    },
+    {
+        question: "What is the primary goal of the Construction Phase Plan (CPP) in CDM 2015?",
+        answers: [
+            {
+                answer: "To allocate project funds",
+                correct: false
+            },
+            {
+                answer: "To create architectural drawings",
+                correct: false
+            },
+            {
+                answer: "To identify health and safety risks during construction",
+                correct: true
+            },
+            {
+                answer: "To schedule project meetings",
+                correct: false
+            }
+        ]
+    },
+    {
+        question: "In CDM 2015, who is responsible for notifying the Health and Safety Executive (HSE) of a construction project?",
+        answers: [
+            {
+                answer: "Client",
+                correct: false
+            },
+            {
+                answer: "Principal Contractor",
+                correct: false
+            },
+            {
+                answer: "Designer",
+                correct: false
+            },
+            {
+                answer: "Principal Designer",
+                correct: true
+            }
+        ]
+    },
+    {
+        question: "What is the role of the 'Worker' in CDM 2015 concerning health and safety on a construction site?",
+        answers: [
+            {
+                answer: "To manage the project budget",
+                correct: false
+            },
+            {
+                answer: "To oversee the project schedule",
+                correct: false
+            },
+            {
+                answer: "To carry out work safely and report hazards",
+                correct: true
+            },
+            {
+                answer: "To design the project's architectural elements",
+                correct: false
+            }
+        ]
+    },
+    {
+        question: "Who is primarily responsible for coordinating health and safety during the construction phase of a project according to CDM 2015?",
+        answers: [
+            {
+                answer: "Client",
+                correct: false
+            },
+            {
+                answer: "Principal Contractor",
+                correct: false
+            },
+            {
+                answer: "Designer",
+                correct: false
+            },
+            {
+                answer: "Principal Designer",
+                correct: true
+            }
+        ]
+    },
+    {
+        question: "What is the purpose of the Health and Safety File in CDM 2015?",
+        answers: [
+            {
+                answer: "To keep a record of project finances",
+                correct: false
+            },
+            {
+                answer: "To store architectural drawings",
+                correct: false
+            },
+            {
+                answer: "To document all accidents on site",
+                correct: false
+            },
+            {
+                answer: "To provide health and safety information for future maintenance",
+                correct: true
             }
         ]
     }
 ]
+
 
 
