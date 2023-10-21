1f7ff2d1def45d4b8b77bbe4e034d4b885456192
Aleks Atr <atraleks@gmail.com>
2023-10-21 12:37:26 +0100

    fix: incorporate jshint warnings

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,34 +1,34 @@
 //import quiz questions
-import { questions } from './questions.js';
+import { questions } from "./questions.js";
 
-const start = 'Start';
-const next = 'Next';
-const header = document.getElementById('header');
-const cdmIntro = document.getElementById('cdm-intro');
-const cdmQuizContainer = document.getElementById('cdm-quiz-container');
-const body = document.getElementById('body');
+const start = "Start";
+const next = "Next";
+const header = document.getElementById("header");
+const cdmIntro = document.getElementById("cdm-intro");
+const cdmQuizContainer = document.getElementById("cdm-quiz-container");
+const body = document.getElementById("body");
 
 const numOfQuestions = questions.length;
-const startButton = document.getElementById('start-btn');
-const quizQuestion = document.getElementById('cdm-question');
-const quizAnswers = document.getElementById('cdm-answers');
-const answerButtons = document.getElementsByClassName('btn');
-const quizExitButton = document.getElementById('exit-btn');
-const quizNextButton = document.getElementById('next-btn');
-const scoreWindow = document.getElementById('score');
+const startButton = document.getElementById("start-btn");
+const quizQuestion = document.getElementById("cdm-question");
+const quizAnswers = document.getElementById("cdm-answers");
+const answerButtons = document.getElementsByClassName("btn");
+const quizExitButton = document.getElementById("exit-btn");
+const quizNextButton = document.getElementById("next-btn");
+const scoreWindow = document.getElementById("score");
 
 
 let questionNumber;
 let questionIndex;
 let score;
 
-quizExitButton.addEventListener('click', () => {
-    window.location.href="../index.html" 
+quizExitButton.addEventListener("click", () => {
+    window.location.href = "../index.html";
 });
-quizNextButton.addEventListener('click', () => {
+quizNextButton.addEventListener("click", () => {
     buttonsControl(next);
 });
-startButton.addEventListener('click', startQuiz);
+startButton.addEventListener("click", startQuiz);
 
 
 function resetQuestionContainer() {
@@ -41,88 +41,99 @@ function startQuiz() {
     score = 0;
     questionNumber = questionIndex + 1;
     showScore(score);
-    quizNextButton.style.display = 'none';
+    quizNextButton.style.display = "none";
     buttonsControl(start);
 }
 
 function showQuestion() {
-    quizNextButton.style.display = 'none';
+    quizNextButton.style.display = "none";
     resetQuestionContainer();
     // declare a constant with assigned value from the questions array (imported from module)
-    const currentQuestion = questions[questionIndex]['question'];
+    const currentQuestion = questions[questionIndex].question;
     // create a template for the html question element
     const questionTemplate = `${questionNumber}. ${currentQuestion}`;
     // update html question element with current question
     quizQuestion.innerHTML = questionTemplate;
-
+    showAnswers();
+}
 
     // declare a constant with assigned value from the questions/answers arrays (imported from module)
-    const currentAnswer = questions[questionIndex]['answers'];
+function showAnswers() {
+    const currentAnswer = questions[questionIndex].answers;
     // create possible answers for the current question
-    for (const index in currentAnswer) {
-        let answer = currentAnswer[index]['answer'];
-        let isCorrect = currentAnswer[index]['correct'];
-        //console.log("I am 'correct' in the currentAnswer - ", correct)
+    for (let i = 0; i < currentAnswer.length; i++) {
+        let answer = currentAnswer[i].answer;
+        let isCorrect = currentAnswer[i].correct;
         const answerTemplate = `
-            <button correct="${isCorrect}" class='btn'>${answer}</button>
+            <button correct="${isCorrect}" class="btn">${answer}</button>
         `;
         quizAnswers.innerHTML += answerTemplate;
     }
     applyEventListener();
 }
 
+
 function applyEventListener() {
-    const answerButtons = document.getElementsByClassName('btn');
+    const answerButtons = document.getElementsByClassName("btn");
     for (let i = 0; i < answerButtons.length; i++) {
-        answerButtons[i].addEventListener('click', checkAnswer);
+        answerButtons[i].addEventListener("click", answerBtnControl);
     }
 }
 
-function checkAnswer(event) {
-    // disable answer buttons 
-    const array = Array.prototype.slice.call(answerButtons);
-    array.forEach(button => {button.disabled = true;})
-
+// Provides user selected answer button response
+function answerBtnControl(event) {
     const selectedBtn = event.target;
-    const isCorrectAnswer = selectedBtn.getAttribute('correct') === 'true';
-    
-    if (isCorrectAnswer) {
-        selectedBtn.classList.add('correct');
-        score += 1;
-        showScore(score)
-    } else {
-        selectedBtn.classList.add('incorrect');
-        quizAnswers.querySelector('[correct="true"]').classList.add('correct');
-    }
-
+    checkAnswer(selectedBtn);
+    disableButton();
     questionIndex += 1;
     questionNumber += 1;
-    quizNextButton.style.display = 'block';
+    quizNextButton.style.display = "block";
+}
+
+//disable answer buttons after user selecting the answer
+function disableButton() {
+    // disable answer buttons 
+    const array = Array.prototype.slice.call(answerButtons);
+    array.forEach(button => { button.disabled = true; });
+    
+}
+
+function checkAnswer(selectedBtn) {
+    const isCorrectAnswer = selectedBtn.getAttribute("correct") === "true";
+
+    if (isCorrectAnswer) {
+        selectedBtn.classList.add("correct");
+        score += 1;
+        showScore(score);
+    } else {
+        selectedBtn.classList.add("incorrect");
+        quizAnswers.querySelector("[correct='true']").classList.add("correct");
+    }
 }
 
 
 function showScore(score) {
     scoreWindow.innerHTML = `
         Score: ${score} of ${numOfQuestions}
-    `
+    `;
 }
 
 function scoreResult(score, numOfQuestions) {
-    let resultFeedback = '';
-    const results = Math.round((score/numOfQuestions)*100);
-    if (results === 100) {
-        resultFeedback = 'Congratulations on your outstanding achievement! Scoring 100% in the quiz is a testament to your dedication and knowledge. Keep up the excellent work!'
-    } else if (results >= 70 && results < 100) {
-        resultFeedback = "Congratulations on completing the quiz! You're doing great, and your effort is commendable. To improve even further, consider reviewing the material in the near future."
+    let resultFeedback = "";
+    const results = Math.round((score / numOfQuestions) * 100);
+    if (results >= 90) {
+        resultFeedback = "Congratulations on your outstanding achievement! The acieved score is a testament to your dedication and knowledge. Keep up the excellent work!";
+    } else if (results >= 70 && results < 90) {
+        resultFeedback = "Congratulations on completing the quiz! You're doing great, and your effort is commendable. To improve even further, consider reviewing the material in the near future.";
     } else {
-        resultFeedback = "Great job on completing the quiz! It's the first step to excelling yourself. As you scored less than 70%, you should revisit the material as soon as possible to reinforce your regulations understanding. Keep up the good work!"
+        resultFeedback = "Great job on completing the quiz! It's the first step to excelling yourself. As you scored less than 70%, you should revisit the material as soon as possible to reinforce your regulations understanding. Keep up the good work!";
     }
     return resultFeedback;
 }
 
 function showResult() {
-    quizNextButton.style.display = 'none';
-    quizQuestion.style.display = 'none';
+    quizNextButton.style.display = "none";
+    quizQuestion.style.display = "none";
 
     resetQuestionContainer();
     let addFeedback = scoreResult(score, numOfQuestions);
@@ -131,21 +142,17 @@ function showResult() {
 
 function buttonsControl(type) {
     if (type == start) {
-        header.style.display = 'none';
-        cdmIntro.style.display = 'none';
-        
+        header.style.display = "none";
+        cdmIntro.style.display = "none";
+
         showQuestion();
         cdmQuizContainer.style.display = "block";
         body.classList.add("body-image");
     } else {
         if (questionIndex < (numOfQuestions)) {
             showQuestion();
-        } else { 
-            showResult()
-            startButton.innerHTML = 'Start Again';
-            startButton.style.display = 'block'
-            startButton.removeEventListener('click', () => {buttonsControl(next)})
-            startButton.addEventListener('click', startQuiz);
+        } else {
+            showResult();
         }
     }
 }
